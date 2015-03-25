d68b7501204974bde797de7632d4029f08c572d5
myfluxi <linflux@arcor.de>
2015-03-25 23:16:32 +0100

    hammerhead: Update display HAL
    https://github.com/CyanogenMod/android_hardware_qcom_display
    commit 19c4ab9829ac810413424f384ab107abd9a1c27f
    
    Change-Id: Iad2e9d8da782be36e5176c333717e06069d13420

--- display/common.mk
+++ display/common.mk
@@ -1,12 +1,12 @@
 #Common headers
-common_includes := device/lge/hammerhead/display/libgralloc
-common_includes += device/lge/hammerhead/display/liboverlay
-common_includes += device/lge/hammerhead/display/libcopybit
-common_includes += device/lge/hammerhead/display/libqdutils
-common_includes += device/lge/hammerhead/display/libhwcomposer
-common_includes += device/lge/hammerhead/display/libexternal
-common_includes += device/lge/hammerhead/display/libqservice
-common_includes += device/lge/hammerhead/display/libvirtual
+common_includes := $(call project-path-for,qcom-display)/libgralloc
+common_includes += $(call project-path-for,qcom-display)/liboverlay
+common_includes += $(call project-path-for,qcom-display)/libcopybit
+common_includes += $(call project-path-for,qcom-display)/libqdutils
+common_includes += $(call project-path-for,qcom-display)/libhwcomposer
+common_includes += $(call project-path-for,qcom-display)/libexternal
+common_includes += $(call project-path-for,qcom-display)/libqservice
+common_includes += $(call project-path-for,qcom-display)/libvirtual
 
 ifeq ($(TARGET_USES_POST_PROCESSING),true)
     common_flags     += -DUSES_POST_PROCESSING
